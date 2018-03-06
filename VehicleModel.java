9a982b059eb343630ced274f894845478629354d
sasmaq <37103211+sasmaq@users.noreply.github.com>
2018-03-06 12:45:05 +0300

    Add files via upload

---
+++ VehicleModel.java
@@ -0,0 +1,6 @@
+
+public class VehicleModel {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ VehicleModelDriver.java
@@ -0,0 +1,7 @@
+
+public class VehicleModelDriver {
+	public static void main(String[] args) {
+		
+	}
+
+}
