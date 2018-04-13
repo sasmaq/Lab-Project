0b39b2363719695885a2bc94fa9a5300008e4abf
sasmaq <37103211+sasmaq@users.noreply.github.com>
2018-04-13 22:35:21 +0300

    Update VehicleModel.java

--- VehicleModel.java
+++ VehicleModel.java
@@ -1,6 +1,59 @@
 
 public class VehicleModel {
-	public static void main(String[] args) {
-		
+	private String modelName;
+	private int stockQuantity,numRentedVehicles;
+	private double rentalPricePerDay;
+		public VehicleModel(String modelName, int stockQuantity, int numRentedVehicles, double rentalPricePerDay) throws IllegalArgumentException{
+			if (stockQuantity<0 || stockQuantity<0 || rentalPricePerDay<0) throw new IllegalArgumentException();
+			this.modelName = modelName;
+			this.stockQuantity = stockQuantity;
+			this.numRentedVehicles = numRentedVehicles;
+			this.rentalPricePerDay = rentalPricePerDay;
+		}
+	
+	
+	public String getModelName() {
+		return modelName;
+	}
+	public int getStockQuantity() {
+		return stockQuantity;
+	}
+	public int getNumRentedVehicles() {
+		return numRentedVehicles;
+	}
+	public double getRentalPricePerDay() {
+		return rentalPricePerDay;
+	}
+	public void setStockQuantity (int stockQuantity) throws IllegalArgumentException{
+		if(stockQuantity<0) throw new IllegalArgumentException();
+		else this.stockQuantity=stockQuantity;
+	}
+	public void setRentalPricePerDay(double rentalPricePerDay) throws IllegalArgumentException{
+		if(rentalPricePerDay<0) throw new IllegalArgumentException();
+		else this.rentalPricePerDay=rentalPricePerDay;
+	}
+	public double rentVehicleModel(int numVehicles, int numDays) throws IllegalArgumentException{
+		if(numVehicles<0 || numDays<0) throw new IllegalArgumentException();
+		else {
+		double cost;
+		cost = numVehicles*(rentalPricePerDay*numDays);
+		return cost;
+		}
+	}
+	public void getVehiclesFromCustomer(int numVehicles) throws IllegalArgumentException{
+		if(numVehicles<0) throw new IllegalArgumentException();
+		else {
+		stockQuantity = stockQuantity + numVehicles;
+		numRentedVehicles = numRentedVehicles - numVehicles;
+		}
+	}
+	public String toString() {
+		String line;
+		line = String.format("%s%t%d%t%d%t%.2f",modelName , stockQuantity , numRentedVehicles , rentalPricePerDay);
+		return line;
+	}
+	public boolean equals(Object  obj) {
+		boolean check = false;
+		return check;
 	}
 }
