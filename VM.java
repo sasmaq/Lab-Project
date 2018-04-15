39f069c1af8f574b9a5d82d9597870cbf06b836d
sasmaq <37103211+sasmaq@users.noreply.github.com>
2018-04-15 06:27:00 +0300

    Update VM.java

--- VM.java
+++ VM.java
@@ -1,59 +1,82 @@
 
-public class VM {
+
+class VehicleModel {
 	private String modelName;
-	private int stockQuantity,numRentedVehicles;
+	private int stockQuantity, numRentedVehicles;
 	private double rentalPricePerDay;
-		public VehicleModel(String modelName, int stockQuantity, int numRentedVehicles, double rentalPricePerDay) throws IllegalArgumentException{
-			if (stockQuantity<0 || stockQuantity<0 || rentalPricePerDay<0) throw new IllegalArgumentException();
-			this.modelName = modelName;
-			this.stockQuantity = stockQuantity;
-			this.numRentedVehicles = numRentedVehicles;
-			this.rentalPricePerDay = rentalPricePerDay;
-		}
-	
-	
+
+	public VehicleModel(String modelName, int stockQuantity, int numRentedVehicles, double rentalPricePerDay)
+			throws IllegalArgumentException {
+		if (stockQuantity < 0 || stockQuantity < 0 || rentalPricePerDay < 0)
+			throw new IllegalArgumentException();
+		this.modelName = modelName;
+		this.stockQuantity = stockQuantity;
+		this.numRentedVehicles = numRentedVehicles;
+		this.rentalPricePerDay = rentalPricePerDay;
+	}
+
 	public String getModelName() {
 		return modelName;
 	}
+
 	public int getStockQuantity() {
 		return stockQuantity;
 	}
+
 	public int getNumRentedVehicles() {
 		return numRentedVehicles;
 	}
+
 	public double getRentalPricePerDay() {
 		return rentalPricePerDay;
 	}
-	public void setStockQuantity (int stockQuantity) throws IllegalArgumentException{
-		if(stockQuantity<0) throw new IllegalArgumentException("Invalid stock quantity");
-		else this.stockQuantity=stockQuantity;
+
+	public void setStockQuantity(int stockQuantity) throws IllegalArgumentException {
+		if (stockQuantity <= 0)
+			throw new IllegalArgumentException("Invalid stock quantity");
+		else
+			this.stockQuantity = stockQuantity;
 	}
-	public void setRentalPricePerDay(double rentalPricePerDay) throws IllegalArgumentException{
-		if(rentalPricePerDay<0) throw new IllegalArgumentException("Invalid rental price per day.");
-		else this.rentalPricePerDay=rentalPricePerDay;
+
+	public void setRentalPricePerDay(double rentalPricePerDay) throws IllegalArgumentException {
+		if (rentalPricePerDay <= 0)
+			throw new IllegalArgumentException("Invalid rental price per day.");
+		else
+			this.rentalPricePerDay = rentalPricePerDay;
 	}
-	public double rentVehicleModel(int numVehicles, int numDays) throws IllegalArgumentException{
-		if(numVehicles<0 || numDays<0) throw new IllegalArgumentException();
+
+	public double rentVehicleModel(int numVehicles, int numDays) throws IllegalArgumentException {
+		if (numDays <= 0)
+			throw new IllegalArgumentException("Invalid number of days.");
+		else if(numVehicles<=0) 
+			throw new IllegalArgumentException("Invalid number of Vehicles.");
+		else if(numVehicles>stockQuantity)
+			throw new IllegalArgumentException("Number of vehicles > stock quantity.");
 		else {
-		double cost;
-		cost = numVehicles*(rentalPricePerDay*numDays);
-		return cost;
+			double cost;
+			cost = numVehicles * (rentalPricePerDay * numDays);
+			return cost;
 		}
 	}
-	public void getVehiclesFromCustomer(int numVehicles) throws IllegalArgumentException{
-		if(numVehicles<0) throw new IllegalArgumentException();
+
+	public void getVehiclesFromCustomer(int numVehicles) throws IllegalArgumentException {
+		if (numVehicles <= 0 || numVehicles>numRentedVehicles)
+			throw new IllegalArgumentException("Invalid number of vehicles.");
 		else {
-		stockQuantity = stockQuantity + numVehicles;
-		numRentedVehicles = numRentedVehicles - numVehicles;
+			stockQuantity = stockQuantity + numVehicles;
+			numRentedVehicles = numRentedVehicles - numVehicles;
 		}
 	}
+
 	public String toString() {
-		String line;
-		line = String.format("%-20s%30s%40s%40s%n%n",modelName , stockQuantity , numRentedVehicles , rentalPricePerDay);
+		String line = String.format("%-20s%30s%40s%40s", modelName, stockQuantity, numRentedVehicles,
+				rentalPricePerDay);
 		return line;
 	}
-	public boolean equals(Object  obj) {
+
+	public boolean equals(Object obj) {
 		boolean check = false;
+		if (modelName.toLowerCase().equals(obj)) check = true;
 		return check;
 	}
 }
